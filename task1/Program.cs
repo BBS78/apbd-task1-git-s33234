ebc5143cbe5b002407c37ee085d08ec46e0d0db6
Bohdan Filonenko <s33234@pjwstk.edu.pl>
2026-03-16 23:08:19 +0100

    Added class Cat

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Cat
+{
+    string name;
+    int age;
+    string gender;
+    string favouriteFood;
+
+    public Cat(string name, int age, string gender, string favouriteFood)
+    {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.favouriteFood = favouriteFood;
+    }
+}
