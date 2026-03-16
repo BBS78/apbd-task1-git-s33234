5ce8dd1c6028ef1caaa29bf2b698940f9165030e
Bohdan Filonenko <s33234@pjwstk.edu.pl>
2026-03-16 23:25:13 +0100

    Added PrintInfo method and Parents field to Cat class

--- task1/Program.cs
+++ task1/Program.cs
@@ -2,17 +2,27 @@
 
 class Cat
 {
-    string name;
-    int age;
-    string gender;
-    string favouriteFood;
+    string? name;
+    int? age;
+    string? gender;
+    string? favouriteFood;
+    Cat[]? parents = new Cat[2];
 
-    public Cat(string name, int age, string gender, string favouriteFood)
+    public Cat(string name, int age, string gender, string favouriteFood, Cat[] parents)
     {
         this.name = name;
         this.age = age;
         this.gender = gender;
         this.favouriteFood = favouriteFood;
+        this.parents = parents;
+    }
+
+    public Cat() {
+        this.name = null;
+        this.age = null;
+        this.gender = null;
+        this.favouriteFood = null;
+        this.parents = null;
     }
 
     void Eat(string food)
@@ -25,9 +35,20 @@ class Cat
             Console.Write("didn't like that.");
     }
 
+    void PrintInfo() {
+        Console.WriteLine($"Name: {name}\n" +
+            $"Age: {age}\n" +
+            $"Gender: {gender}\n"+
+            $"Favourite Food: {favouriteFood}\n" +
+            $"Parents: {parents[0].name}, {parents[1].name}");
+    }
+
     static void Main(string[] args)
     {
-        Cat c = new Cat("Tom", 4, "male", "fish");
-        c.Eat("pizza");
+        Cat mom = new Cat("Nina", 20, "female", "fish", [new Cat(), new Cat()]);
+        Cat dad = new Cat("Alan", 22, "male", "pizza", [new Cat(), new Cat()]);
+
+        Cat child = new Cat("Tom", 4, "male", "fish", [mom, dad]);
+        child.Eat("pizza");
     }
 }
