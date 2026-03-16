cfabde76bb5739b737fe2142e910d68441a83167
Bohdan Filonenko <s33234@pjwstk.edu.pl>
2026-03-16 23:43:24 +0100

    Added static method CalculateMax

--- task1/Program.cs
+++ task1/Program.cs
@@ -68,6 +68,15 @@ class Cat
         }
         return sum/values.Length;
     }
+    public static int CalculateMax(int[] values)
+    {
+        int temp = 0;
+        for (int i = 0; i < values.Length; i++)
+        {
+            if (values[i] > temp) { temp = values[i]; }
+        }
+        return temp;
+    }
 
 
     static void Main(string[] args)
@@ -83,5 +92,7 @@ class Cat
         //child.PrintInfo();
 
         Console.WriteLine(Cat.CalculateAverage([1, 2, 3, 4, 5]));
+        Console.WriteLine(Cat.CalculateMax([1, 2, 3, 4, 5]));
+
     }
 }
--- task1/obj/Debug/net10.0/task1.AssemblyInfo.cs
+++ task1/obj/Debug/net10.0/task1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bff9b8757000c59e9b7546dfa1942d48dce89c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+458ebe141dedacd809b30dfe0c91caafd150c83c")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
